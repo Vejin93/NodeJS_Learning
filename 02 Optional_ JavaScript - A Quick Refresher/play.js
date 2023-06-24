1996920b0ff6b7032ff1761b2fdc7ffac7207ca8
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-06-24 10:35:19 +0200

    Error for assignment to constant variable

--- 02 Optional_ JavaScript - A Quick Refresher/play.js
+++ 02 Optional_ JavaScript - A Quick Refresher/play.js
@@ -2,6 +2,8 @@ const name = 'Jelena';
 let age = 30;
 const hasHobbies = true;
 
+//name = 'Jela';  -- TypeError: Assignment to constant variable.
+
 age = 31;
 
 function summerizeUser(userName, userAge, userHasHobby){
