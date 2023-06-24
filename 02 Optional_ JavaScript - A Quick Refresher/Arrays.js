a5392d14fbdb63e38a350b32ed16ca36dc44a286
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-06-24 11:04:41 +0200

    Add 'push' method for adding new array item

--- 02 Optional_ JavaScript - A Quick Refresher/Arrays.js
+++ 02 Optional_ JavaScript - A Quick Refresher/Arrays.js
@@ -1,7 +1,16 @@
 const hobbies = ['Flowers','Movies','Books'];
 
-for (let hobby of hobbies)
-{
+/* for (let hobby of hobbies) {
+    console.log(hobby);
+} */
+
+//console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+
+//console.log(hobbies);
+
+hobbies.push('Yoga');
+
+for (let hobby of hobbies) {
     console.log(hobby);
 }
 
