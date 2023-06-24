ed22d0c90c2c564f391f3b87e936c32b2e14ce16
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-06-24 11:00:12 +0200

    Work with arrays and arrays methods

---
+++ 02 Optional_ JavaScript - A Quick Refresher/Arrays.js
@@ -0,0 +1,10 @@
+const hobbies = ['Flowers','Movies','Books'];
+
+for (let hobby of hobbies)
+{
+    console.log(hobby);
+}
+
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+
+console.log(hobbies);
