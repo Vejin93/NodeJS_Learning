5105151c5782c1cb9322facb1c0f7cee4a481753
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-06-24 10:33:37 +0200

    Difference between var, const and let

--- 02 Optional_ JavaScript - A Quick Refresher/play.js
+++ 02 Optional_ JavaScript - A Quick Refresher/play.js
@@ -1,9 +1,11 @@
-var name = 'Jelena';
-var age = 30;
-var hasHobbies = true;
+const name = 'Jelena';
+let age = 30;
+const hasHobbies = true;
+
+age = 31;
 
 function summerizeUser(userName, userAge, userHasHobby){
-    return ('Name is ' + userName + ', age is ' + userAge + ' and user has hobbies ' + userHasHobby); 
+    return ('Name is ' + userName + ', age is ' + userAge + ' and user has hobbies: ' + userHasHobby); 
 }
 
 console.log(summerizeUser(name,age,hasHobbies)); 
