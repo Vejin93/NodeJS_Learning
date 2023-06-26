09a463c2c0efffe3653bfe9e8db6b722b678acd0
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-06-26 20:14:38 +0200

    Add route for 404-Page not found

--- 05 Working with Express.js/app.js
+++ 05 Working with Express.js/app.js
@@ -11,8 +11,11 @@ const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(shopRoutes);
 app.use(adminRoutes);
+app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+});
 
 app.listen(3000);
