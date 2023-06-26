5c37dae43b628306fa9880ba8493f77773b2fb09
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-06-26 20:09:01 +0200

    Add routes folder and admin and shop routes

--- 05 Working with Express.js/app.js
+++ 05 Working with Express.js/app.js
@@ -1,23 +1,18 @@
 const http = require('http'); 
 
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(shopRoutes);
+app.use(adminRoutes);
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-});
 
 app.listen(3000);
