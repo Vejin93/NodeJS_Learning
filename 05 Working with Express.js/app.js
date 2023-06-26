bf4f925aab29644b28d9e6d706effae06a3c3fce
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-06-26 18:58:35 +0200

    Replace old code with Express

--- 05 Working with Express.js/app.js
+++ 05 Working with Express.js/app.js
@@ -1,6 +1,17 @@
 const http = require('http'); 
+
 const express = require('express');
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log("In another middleware!");
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen();
