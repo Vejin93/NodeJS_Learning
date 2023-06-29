402512769ab198cb6a007f04faf4b2dbb19e0ab2
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-06-29 19:57:41 +0200

    Add 'pug' in code

--- 05 Working with Express.js/routes/shop.js
+++ 05 Working with Express.js/routes/shop.js
@@ -4,10 +4,16 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views','shop.html'));
+    //console.log(adminData.products);
+    //res.sendFile(path.join(rootDir, 'views','shop.html'));
+    
+    //Add pug
+    res.render('shop.pug');
 });
 
 module.exports = router;
